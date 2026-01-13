327588a651a49a250e052dda646eb0a680451544
jess <jkant@bu.edu>
2026-01-12 21:54:08 -0500

    added enumerator function

--- detector.sh
+++ detector.sh
@@ -5,6 +5,8 @@
 # jessdkant.bsky.social
 #
 
+
+
 printf "\n\t github.com/jessicakay/glossy\n"
 read -p $'\n\t ~ Target (url): ' targ
 read -p $'\n\t ~ choose filename prefix: ' outNAME
@@ -23,6 +25,16 @@ esac
 buffer="$(curl -s $targ)"
 detect_m3u8="$(echo $buffer | sed 's/\"/\n/g' | grep  -Po "http.*m3u8" | uniq | wc -l)"
 
+function file_enumerater (){
+	read -p $'\n\t ~ enumerate [y/n]?: ' enum_streams
+	if [[ $enum_streams == "y" ]]; then
+		printf "\n" && echo $buffer |  tr "\"" "\n" > counter_temp
+		cat counter_temp | tr "\"" "\n" | grep -P "https.*?[^\":]\....?.$" | \
+		grep -Po "\.[pm]..?[^\/]$" | sort | uniq -c
+		printf "\n"
+	fi
+	}
+
 if ! [[ -z $( echo $targ | grep "sliq" ) ]];
 	then printf "\n\t-> Sliq platform detected"
 	platform_type="sliq"
@@ -36,24 +48,24 @@ else
 	printf "\n\t-! platform not detected, searching for streams..."
 fi
 
-if [[ $detect_m3u8 == 1 ]]; then
-	printf "\n\t-> detected 1 stream type=m3u8\n \n\t~ attempting download...\n"
-	case "$file_fmt" in
-		v) ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) -c copy $outNAME.mp4 ; return ;;
-		a) ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) -vn -ac 2 -b:a 192k $outNAME.mp3 ; return ;;
-		b) ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) -c copy $outNAME.mp4 &&
-			ffmpeg -i $outNAME.mp4 -c:a -acodec libmp3lame -vn $outname.mp3
-			return ;;
-	esac
+if  [[ -n $( echo $buffer | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) ]]; then
+	if [[ $detect_m3u8 == 1 ]]; then
+		printf "\n\t-> detected 1 stream type=m3u8\n \n\t~ attempting download...\n"
+		case "$file_fmt" in
+			v) ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) -c copy $outNAME.mp4 ; return ;;
+			a) ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) -vn -ac 2 -b:a 192k $outNAME.mp3 ; return ;;
+			b) ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" | uniq ) -c copy $outNAME.mp4 &&
+				ffmpeg -i $outNAME.mp4 -c:a -acodec libmp3lame -vn $outname.mp3
+				return ;;
+		esac
+	else
+		printf "\n\t-! stream not found... \n";
+		file_enumerater
+		return;
+	fi
 elif [[ $detect_m3u8 > 1 ]]; then
 	printf "\n\t-> detected $detect_m3u8 m3u8 streams\n\t"
-	read -p " ~ enumerate [y/n]?: " enum_streams
-	if [[ $enum_streams == "y" ]]; then
-		printf "\n" && echo $buffer |  tr "\"" "\n" > counter_temp
-		cat counter_temp | tr "\"" "\n" | grep -P "https.*?[^\":]\....?.$" | \
-		grep -Po "\.[pm]..?[^\/]$" | sort | uniq -c
-		printf "\n"
-	fi
+	file_enumerater
 	read -p $'\t ~ multiple streams located, use -m 1 [y/n]?' m_one
 	printf "\n\n"
 	if [[ $m_one == "y" ]]; then
@@ -67,6 +79,6 @@ elif [[ $detect_m3u8 > 1 ]]; then
 		ffmpeg -i $(curl $targ | grep "\Khttps.*?m3u?8" -oP | grep "https" -m 1) -c copy $outNAME.mp4
 	fi
 else
-	printf "\n\t-> no playlist found..."
+	printf "\n\t-> no playlist found...\n"
 fi
 
