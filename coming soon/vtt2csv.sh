a0746a128da3e5d1da6d2d3f40909f3811267b4d
jess <jkant@bu.edu>
2026-01-19 19:53:22 -0500

    vtt converter

--- coming soon/vtt2csv.sh
+++ coming soon/vtt2csv.sh
@@ -1,10 +1,24 @@
-read -p "filename (save as):" file_name
-if [[ -z "$(ls -las $file_name )" ]]; then
-    printf "temp exists\n"
-    printf "\n\nfiles found: $(ls -las $file_name )"
- else
-    cat file.vtt | sed 's/\ --> \|\n/\n/g' | awk '{print "\"" $0 "\","}' > "${file_name}"
-    cat temp | sed 's/\"\",//g' | tr "\n" " " | awk '{print "\t" $0}' | sed  's/  /\n/g' >  "${file_name}".csv
-    # rm temp
-    head  -n 1 "${file_name}"
+# glossy vtt to csv converter
+# jessdkant.bsky.social
+#
+
+# dos2unix -n file.vtt file2.vtt
+# cat file2.vtt | sed 's/\ --> \|\n/\n/g' | awk '{print "\"" $0 "\","}' > temp
+# cat temp | sed 's/\"\",//g' | tr "\n" " " | awk '{print "\t" $0}' | sed  's/  /\n/g' >  temp.csv
+
+read -p $'\nsource URL: ' sourceURL
+read -p $'\nfilename (save as):' file_name
+if [ -f "temp.vtt" ]; then
+    printf "temp.vtt already exists... exiting."
+    return
+else
+    curl -q $sourceURL > temp.vtt
 fi
+
+dos2unix temp.vtt
+cat temp.vtt | sed 's/\ --> \|\n/\n/g' | awk '{print "\"" $0 "\","}' > "${file_name}"
+cat $file_name | sed 's/\"\",//g' | tr "\n" " " |
+    awk '{print "\t" $0}' | sed  's/  /\n/g' >  "${file_name}"_temp.csv
+tail -n +2 "${file_name}"_temp.csv > "${file_name}".csv
+head  -n 5 "${file_name}".csv
+
