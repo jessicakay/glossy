d8cc6dbe6075454421d1f147e8725f4c4a133a8d
jess <jkant@bu.edu>
2026-02-04 13:15:18 -0500

    better segmenting

--- coming soon/webvtt_convert.py
+++ coming soon/webvtt_convert.py
@@ -5,7 +5,6 @@ import os
 import pathlib
 from shlex import join
 from traceback import print_tb
-
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
@@ -44,24 +43,37 @@ num_file=0
 for file in os.listdir() :
     if file.endswith(".vtt") :
         num_file=num_file+1
-        if num_file>5 :
-            print("processing "+str(num_file)+": "+(file)+"...")
+        print("processing "+str(num_file)+": "+(file)+"...")
         temp=webvtt.read(file)
         cap_text=[]
+        transcript_line = []
         paranum=0
         for caption in temp:
             cap_text.append(caption.raw_text)
             outname = ''.join([file.removesuffix(".vtt"),".txt"])
             if Path(outname).exists():
+                file_year = str(file)[0:4]
+                file_month = str(file)[5:6]
+                file_day = str(file)[7:8]
+                file_date = str(''.join([file_month, file_day, file_year]))
                 with open(outname, "a") as f:
                    f.write((caption.raw_text).lower()+" ")
-                   if paranum<50 :
+                   if paranum<20 :
                        paranum=paranum+1
+                       transcript_line.append(caption.raw_text)
                    else :
+                       with open(''.join([file.removesuffix(".vtt"), ".csv"]), "a", newline='') as csvfile:
+                           csveet = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL,
+                                               dialect='unix')
+                           csveet.writerow([file_date, file_month, file_day, file_year,
+                                            caption.start, caption.end,
+                                            str(' '.join(transcript_line)).lower()])
                        f.write('\n'+str(caption.start)+'\n\n')
                        paranum=0
+                       transcript_line=[]
                    f.close()
             else:
                 with open(outname, "w") as f:
                     f.write("============== transcript generated from "+file+"==============\n\n")
                     f.close()
+        print("\n ~ read " + str(len(cap_text)) + " lines\n")
